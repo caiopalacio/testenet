0fac225c53ef8fa277ac1d6952172972afb11bb8
Caio <94841685+caiopalacio@users.noreply.github.com>
2022-06-06 23:52:21 -0300

    Add files via upload

---
+++ Data/BuscaImoveis.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace teste.Data
+
+{
+    public class BuscaImoveis
+    {
+        private readonly string _imovelService;
+        
+
+        public BuscaImoveis(IConfiguration configuration)
+        {
+            _imovelService = configuration.GetSection("API").GetSection("BaseUrl").Value;
+            
+        }
+
+        //getordes envia o que é solicitado pela api
+        public async Task<List<ListaImoveis>> GetOrders()
+        {
+            try
+            {
+                RestClient client = new($"{_imovelService}/apiteste");
+                RestRequest request = new();
+
+
+               
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Content-Type", "application/json");
+
+                RestResponse response = await client.ExecuteGetAsync(request);
+                
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<ListaImoveis> ImovelServiceModelResponse = JsonConvert.DeserializeObject<List<ListaImoveis>>(response.Content);
+
+                    return ImovelServiceModelResponse;
+                }
+                else
+                    return null;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+
+
+        }
+    }
+}
---
+++ Data/ListaImoveis.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace teste.Data
+{
+    public class ListaImoveis
+    {
+        public int Id { get; set; }
+        public string TipoImovel { get; set; }
+        public decimal ValorVenda { get; set; }
+        public decimal ValorLocacao { get; set; }
+        public decimal Area { get; set; }
+        public string Endereco { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public int Cep { get; set; }
+
+    }
+}
